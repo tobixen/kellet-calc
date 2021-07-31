b0ec4ff9fbfb057e47abcccf9bf1ea4ec52b6c35
Tobias Brox <tobias@redpill-linpro.com>
2021-07-31 13:54:24 +0200

    eksempler

---
+++ eksempler.py
@@ -0,0 +1,19 @@
+from kellet_calc import daumann_kalk
+
+taulengde=30 # meter
+dybde=10 # meter
+daumannsvekt=10 # kg
+for daumannlengde in (1, 5, 10, 15, 20, 25, 29):
+    print()
+    print("Putter ut %i meter med tau til daumann" % daumannlengde)
+    ## hvor dypt vil daumannen befinne seg dersom tauet er helt stramt?
+    expected_y_pos = dybde/taulengde*(taulengde-daumannlengde)
+    ## max_dausynk - når tauet henger rett ned fra baugen, eller når daumann har truffet sjøbunnen
+    max_dausynk = min(daumannlengde-(dybde-expected_y_pos), expected_y_pos)
+    for dausynk in (0.01, 0.1, 0.5*max_dausynk, max_dausynk-0.01):
+        results = daumann_kalk(daumannsvekt, taulengde, dybde, daumannlengde, dausynk)
+        print("=====")
+        print("Dausynk: %.2f m" % results['dausynk'])
+        print("Vindkraft på båten: g * %.2f kg" % results['Bx'])
+        print("Vinkel på ankerkraft (y:l - bør være mindre enn 1:3): 1:%.2f" % (results['l1']/results['l1y']))
+        
--- kellet_calc.py
+++ kellet_calc.py
@@ -5,15 +5,15 @@ except:
     assert_almost_equal = lambda x, y: True
     
 
-def daumann_kalk(ankertaulengde, dybde, daumannlengde, dausynk, daumannsvekt):
+def daumann_kalk(daumannsvekt, ankertaulengde, dybde, daumannlengde, dausynk):
     l1 = ankertaulengde - daumannlengde
     l2 = daumannlengde
     l2y = dybde*l2/ankertaulengde+dausynk
     l1y = dybde - l2y
     Dy = daumannsvekt
-    By = l2y*Dy*sqrt(l1**2-l1y**2)/(l1y*(l2y*sqrt(l1**2-l1y**2)/l1y-sqrt(l2**2-l2y**2)))
     l1x = sqrt(l1**2-l1y**2)
     l2x = sqrt(l2**2-l2y**2)
+    By = l2y*Dy*sqrt(l1**2-l1y**2)/(l1y*(l2y*sqrt(l1**2-l1y**2)/l1y-sqrt(l2**2-l2y**2)))
     Bx = By*sqrt(l2**2-l2y**2)/l2y
     Ay = By-Dy
     Ax = Bx
